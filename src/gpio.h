19a9f2d95dd2eb237893b730dfe9be52c592abbc
David <ea3ihi@gmail.com>
2021-07-04 19:24:13 +0200

    gpio connected

--- src/gpio.h
+++ src/gpio.h
@@ -1,6 +1,6 @@
 
-#define GPIO_CONNECTED_SIGNAL 0
-#define GPIO_DISCONNECTED_SIGNAL 255
+#define GPIO_CONNECTED_SIGNAL 1
+#define GPIO_DISCONNECTED_SIGNAL 0
 
 void gpio_init(void);
 void gpio_deinit(void);
